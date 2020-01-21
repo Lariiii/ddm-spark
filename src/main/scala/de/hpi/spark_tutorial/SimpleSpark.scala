ea0c750965eeb1aa00148fd879515048e457817e
Lariiii <lari-lari@gmx.net>
2020-01-21 21:20:37 +0100

    Refactor and add default data path.

--- src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
+++ src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
@@ -1,11 +1,6 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.DecisionTreeClassificationModel
-import org.apache.spark.ml.classification.DecisionTreeClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.sql.SparkSession
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
@@ -22,7 +17,7 @@ object SimpleSpark extends App {
     //------------------------------------------------------------------------------------------------------------------
 
     var cores = 4
-    var path = "data/TPCH"
+    var path = "./TPCH"
 
     val parser = args.iterator
     while (parser.hasNext) {
@@ -43,9 +38,6 @@ object SimpleSpark extends App {
 
     // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
     spark.conf.set("spark.sql.shuffle.partitions", "8") //
-
-    // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
-    import spark.implicits._
 
     println("---------------------------------------------------------------------------------------------------------")
 
--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,8 +1,7 @@
 package de.hpi.spark_tutorial
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
 
 import scala.collection.mutable.ListBuffer
 
